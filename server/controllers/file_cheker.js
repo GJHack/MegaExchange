fb9aa27b9a2316f8a160abbc927d8b98e2d60302
GJHack <greatjavadev@gmail.com>
2023-12-29 13:56:06 +1000

    create auth and upload

--- server/controllers/file_cheker.js
+++ server/controllers/file_cheker.js
@@ -1,6 +1,7 @@
 'use strict';
 const unzipper = require('unzipper');
 const fs = require('fs')
+const convert = require('xml-js');
 
 const ABS_PATH = "/usr/share/nginx/html/back/public/uploads/exchangeStrapi/tempXMLS/"
 
@@ -23,11 +24,19 @@ module.exports = {
       console.log(`Результат поиска полученного от 1С архива:  ${(searchZipResult) ? "НАЙДЕН" : "НЕ НАЙДЕН"}`);
 
       if(await searchZipResult) {
+
         const resultUnzip = await unzip(ABS_PATH + filename, ABS_PATH, filename)
         console.log("Результат распаковки полученного от 1С архива: " + resultUnzip)
+
         if(await resultUnzip) {
-          const searchFolderResult = await searchInFolder(ctx, ABS_PATH, filename.split('.')[0], false)
+
+          const searchFolderResult = await searchInFolder(ctx, ABS_PATH, filename.split('.')[0], false, false)
           console.log(`Результат поиска папки  с файлами:  ${(await searchFolderResult) ? "НАЙДЕН" : "НЕ НАЙДЕН"}`);
+
+          if(await searchFolderResult) {
+            const searchImportXMLs = await getAllFiles(ABS_PATH, filename.split('.')[0])
+            console.log(`Результат получения всех файлов внутри: ${await searchImportXMLs}`);
+          }
         }
       }
 
@@ -48,9 +57,9 @@ async function unzip(filePath = "",absPath = "/", fileName = "text.zip") {
 
   if(!filePath || typeof filePath !== "string") return
 
-    console.log("старт распаковки....")
+    //console.log("старт распаковки....")
     const newFolderName = fileName.split('.')[0];
-    console.log("Файл будет разархивирован по пути: " + newFolderName)
+    //console.log("Файл будет разархивирован по пути: " + newFolderName)
 
     const promise = new Promise((resolve, reject) => {
       try {
@@ -80,16 +89,16 @@ async function unzip(filePath = "",absPath = "/", fileName = "text.zip") {
  * @param {String} extension - Расширение файла ( по дефолту будет искать папки )
  * @returns {boolean}
  */
-async function searchInFolder(ctx, filePath = "", fileName = "text", extension = "") {
+async function searchInFolder(ctx, filePath = "", fileName = "text", extension = "",getAllNames = false) {
   if(!filePath || typeof filePath !== "string") return
 
   const searchPath = (extension) ? filePath : filePath + fileName;
-
+  console.log(getAllNames)
   try{
     if(!extension) {
-      console.log(`Поиск папки !  ${searchPath}`)
+     // console.log(`Поиск папки !  ${searchPath}`)
       if(fs.existsSync(searchPath)) {
-        console.log(`Папка по пути ${searchPath} cуществует`)
+        //console.log(`Папка по пути ${searchPath} cуществует`)
         ctx.body = JSON.stringify({
           error: false,
           result: true,
@@ -101,26 +110,65 @@ async function searchInFolder(ctx, filePath = "", fileName = "text", extension =
         return false;
       }
     } else {
-      console.log(`Поиск файла в  ${searchPath}`)
-      const promise = new Promise((resolve, reject) => {
-        fs.readdirSync(searchPath).map(checkName => {
-          const formatSearchString = (extension) ? "." + extension : extension
-          const checkString = fileName + formatSearchString
-          if(checkName === checkString) {
+      if(!getAllNames) {
+        console.log("IN SINGLE")
 
-            console.log(`
+        //console.log(`Поиск файла в  ${searchPath}`)
+        const promise = new Promise((resolve, reject) => {
+          fs.readdirSync(searchPath).map(checkName => {
+            const formatSearchString = (extension) ? "." + extension : extension
+            const checkString = fileName + formatSearchString
+            if(checkName === checkString) {
+              console.log(`
                   "Файл" с именем ${fileName} найден!
                      `)
-            console.log(searchPath)
-            resolve(true);
-          }
-      })
-      });
-      return await promise
+              resolve(true);
+            }
+          })
+        });
+        return await promise
+      } else {
+
+      }
+
     }
   } catch(e) {
     console.log(e)
     return false;
   }
 
+}
+async function getAllFiles(absPath = "/", folderName = "test") {
+  const searchPath = absPath + folderName;
+
+  console.log(searchPath)
+
+  const promise = new Promise(async (resolve, reject) => {
+    console.log("Получение всех файлов в папке.")
+    const allFileNames = []
+
+    fs.readdirSync(searchPath).map(async checkName => {
+      const checkString = folderName
+
+      allFileNames.push({
+        name: checkName,
+        dir: searchPath,
+        path: `${searchPath}/${checkName}`,
+        jsonData: await parseXML(`${searchPath}/${checkName}`)
+      })
+    })
+
+    console.log(allFileNames)
+    resolve(allFileNames)
+  });
+
+  return await promise
+}
+
+
+async function parseXML(path = "/") {
+  let result;
+
+  console.log(result);
+  return await result;
 }
